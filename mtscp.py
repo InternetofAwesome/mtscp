250b4858d0a1a8ba4d01b4d067464985e71db05e
Automated Build Server <smandy@quirky.com>
2015-06-29 14:24:07 -0700

    working with typical recursive use.  need to test edge cases, implement command line options.

--- mtscp.py
+++ mtscp.py
@@ -11,23 +11,12 @@ import os
 import md5
 import math
 from urlparse import urlparse
-from IPython.external.ssh.tunnel import paramiko
-from guidata.dataset import datatypes
-from hgext.mq import queue
-from hgext.keyword import files
-from mercurial.demandimport import nothing
-from mutex import mutex
-from samba.dcerpc.initshutdown import SHTDN_REASON_FLAG_PLANNED
-from duplicity.dup_temp import SrcIter
-from httplib import CREATED
-# from Crypto.SelfTest.Random.test__UserFriendlyRNG import multiprocessing
+import paramiko
+from optparse import OptionParser
 
 # Constants
 CHUNK_SIZE =     1024*1024
-THREADS =       1
-host = '127.0.0.1'
-username = 'sam'
-password = ''
+THREADS =       8
 
 def signal_handler(signal, frame):
     print('You pressed Ctrl+C!')
@@ -48,7 +37,6 @@ class File:
         self.src = src
         self.dest = dest
         self.mutex = Lock()
-        
 class LocalFile(File):
     def __init__(self, src, dest):
         File.__init__(self, src, dest)
@@ -67,11 +55,13 @@ class LocalFile(File):
         chunk = Chunk()
         self.mutex.acquire()
         if not self.file:
+            self.mutex.release()
             raise StopIteration()
         chunk.offset = self.file.tell()/CHUNK_SIZE
         chunk.size = -self.file.tell()
         chunk.data = self.file.read(CHUNK_SIZE)
         if not chunk.data:
+            self.mutex.release()
             raise StopIteration()
         chunk.size += self.file.tell()
         self.mutex.release()
@@ -96,6 +86,11 @@ class LocalFile(File):
         m.update(self.file.read(size))
         self.mutex.release()
         return m.digest()
+    def eof(self):
+        self.mutex.acquire()
+        ret = self.file_size == self.file.tell()
+        self.mutex.release()
+        return ret
 #     def dest_created(self, created=None):
 #         ret = False
 #         self.mutex.acquire()
@@ -112,6 +107,7 @@ class LocalDir:
         self.files = []
         self.folders = []
         self.semaphore = Semaphore(THREADS)
+        self.current_file = None
         for root, subFolders, files in os.walk(src):
             print "%s, %s, %s" % (root, subFolders, files)
             dest_dir = os.path.join(dest, os.path.relpath(root, src))
@@ -130,9 +126,12 @@ class LocalDir:
     def __iter__(self):
         return self
     def next(self):
-        if(len(self.files)):
-            return self.files.pop()
-        raise StopIteration()
+        if(self.current_file is None or self.current_file.eof()):
+            if(len(self.files)):
+                self.current_file = self.files.pop()
+            else:
+                raise StopIteration()
+        return self.current_file
 class SSH_Thread(Thread):
     mutex = Lock()
     files = []
@@ -159,6 +158,7 @@ class SSH_Thread(Thread):
         self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         self.ssh.connect(host, username=username, password=password)
     def run(self):
+        print "starting thread " + current_thread().getName()
         self.running = True
         sftp = self.ssh.open_sftp()
         if(sftp is None):
@@ -176,6 +176,7 @@ class SSH_Thread(Thread):
             #if the file doesn't exist, or is the wrong size, create it.
             if(stat is None or stat.st_size != file.file_size):
                 if(stat is None):
+                    print file.dest
                     scpfile = sftp.file(file.dest, 'w+')
                 else:
                     scpfile = sftp.file(file.dest, 'r+')
@@ -193,14 +194,14 @@ class SSH_Thread(Thread):
                 md5 = stdout.readline()[0:32]
                 #if we haven't transferred it, do it until the md5 matches.
                 while(md5 != chunk.md5):
-                    print "trying chunk %d" % chunk.offset
+                    print "trying chunk %d in %s" % (chunk.offset, current_thread().getName())
                     scpfile.seek(chunk.offset*CHUNK_SIZE)
                     scpfile.write(chunk.data)
                     print cmd
                     stdin, stdout, stderr = self.ssh.exec_command(cmd)
                     md5 = stdout.readline()[0:32]
                     print "chunk.md5: %s, md5: %s" %(chunk.md5, md5)
-            file.close();
+            scpfile.close();
             print "leaving thread " + current_thread().getName()
     def stop(self):
         self.running = False
@@ -231,17 +232,16 @@ class SSH_Thread(Thread):
             except:
                 print "some bad shit happened."
                 self.connect()
-   
-print "dickbath"
-path = "/home/sam/Downloads/Dropbox"
-destination = "/home/sam/trash"
+
+path = "/home/sam/Documents/mtscp/src"
+destination = "/home/sam/Documents/mtscp/dest"
 
 file_list = LocalDir(path, destination)
     
 sht = []
 for i in range(0,THREADS):
-    sht.append(SSH_Thread('127.0.0.1', 'sam', '', file_list))
-    if(i==0):
+    sht.append(SSH_Thread('localhost', 'sam', '', file_list))
+    if(i==0 ):
         print "running mkdir on %d" % i
         sht[0].mkdir(file_list.folders)
     sht[i].start()
@@ -250,64 +250,3 @@ for t in sht:
     t.join()
 print "all threads jioned"
 sys.exit()
-    
-# queue = Queue(THREADS+1)
-if __name__ == "__main__":
-    cg = Chunk_Generator("/home/sam/Downloads/Dropbox", "/home/sam/trash")
-#     cg.queue = queue  
-    cg.start()
-#     while(cg.queue.get() or cg.is_alive()):   
-#         pass 
-#     sys.exit()
-
-    for i in range(0,THREADS):
-        sht[i].stop()
-    for i in range(0,THREADS):
-        sht[i].join()
-    cg.join()
-    print "would exit"
-    # sys.exit()
-
-
-# f = SSHFile('freenas.local', 'root', '')
-# chunk = Chunk()
-# chunk.data = "Dickbath fartnose"
-# md5 = md5.new()
-# md5.update(chunk.data)
-# chunk.md5 = md5.digest()
-# chunk.md5 = str(bytearray(chunk.md5)).encode('hex')
-# chunk.chunk_size = len(chunk.data)
-# chunk.path  = "/mnt/main/media/trash"
-# chunk.offset = 0
-# print "calculated source md5: '" + chunk.md5 + "'"
-# f.chunk_write(chunk)
-
-
- 
-# for thread in t: 
-#     thread.join()
-# 
-# ssh = paramiko.SSHClient()
-# ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-# try:
-#     print "creating connection"
-#     ssh.connect(host, username=username, password=password)
-#     print "connected"
-#     stdin, stdout, stderr = ssh.exec_command("dd of=~/test count=1024")
-#     print "writing"
-#     stdin.write("Testing some shit\r\n")
-#     stdin.write("Other shit\r\n")
-#     stdin.channel.shutdown_write();
-#     stdin.flush()
-#     print "reading"
-#     # if stderr.channel.recv_stderr_ready():
-#     print stderr.readlines()	
-#     stdin, stdout, stderr = ssh.exec_command("md5 ~/test")
-#     print stdout.readlines()
-# except KeyboardInterrupt:
-#     print "Bye"
-#     sys.exit()
-# finally:
-#     print "closing connection"
-#     ssh.close()
-#     print "closed"
